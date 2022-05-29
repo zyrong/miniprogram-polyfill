4d845624b8155e96fb02ddb544e714c5c14c2b47
zhaoyr <543434337@qq.com>
2022-05-29 13:27:07 +0800

    build: 增加pnpm install 时进行link

--- gulpfile.ts
+++ gulpfile.ts
@@ -5,6 +5,10 @@ import packageJson from './package.json'
 import fs from 'fs/promises'
 import os from 'os'
 import { delDist, buildCjs, buildEsm, npmlink, buildNpm } from './builds/index'
+import { exec as exec_, ExecOptions, execFile } from 'child_process'
+import { promisify } from 'util'
+import chalk from 'chalk'
+const exec = promisify(exec_)
 
 const pkgRoot = path.join(__dirname, '.')
 const srcGlob = path.join(pkgRoot, './index.ts')
@@ -25,4 +29,77 @@ export async function build(done: TaskCallback) {
   } catch (error: any) {
     done(error)
   }
+}
+
+export async function link(done: TaskCallback) {
+  const log = function (list: any[]) {
+    list.forEach((item) => {
+      if (!item) return
+      if (item.code === 1) {
+        console.log(chalk.red(item.stdout))
+      } else {
+        console.log(chalk.green(item.stdout))
+      }
+    })
+  }
+  // 环境变量不生效时可以手动设置
+  // const PNPM_HOME = '/Users/zhao/Library/pnpm'
+  const execEnvOptions = {
+    env: {
+      ...process.env,
+      // PNPM_HOME,
+      // PATH: `${PNPM_HOME}:${process.env.PATH}`,
+    },
+  }
+
+  // 运行miniprogram-tests的小程序之前先对使用到的包进行link
+  const packagesPath = path.join(pkgRoot, 'packages')
+  const packageDirNameList = await fs.readdir(packagesPath)
+  const createLinkResults = await Promise.all(
+    packageDirNameList.map((dirname) => {
+      const packagePath = path.join(packagesPath, dirname)
+
+      return exec('pnpm link --global', {
+        ...execEnvOptions,
+        cwd: packagePath,
+      }).catch((err) => {
+        return err
+      })
+    })
+  )
+  log(createLinkResults)
+
+  const packageNameList = (
+    await Promise.all(
+      packageDirNameList.map((dirname) => {
+        const packagePath = path.join(packagesPath, dirname)
+        return fs.readFile(path.join(packagePath, 'package.json'))
+      })
+    )
+  ).map((pkgJsonBuf) => {
+    return JSON.parse(pkgJsonBuf.toString()).name
+  })
+
+  const mpTestDirPath = path.join(pkgRoot, 'miniprogram-tests')
+  let mpTestDirList = await fs.readdir(mpTestDirPath)
+  const blackList = ['common', 'test-lib.ts']
+  mpTestDirList = mpTestDirList.filter(
+    (dirname) => !blackList.includes(dirname)
+  )
+  const linkPkgResults = await Promise.all(
+    mpTestDirList
+      .map((dirname) => {
+        const mpTestProjectPath = path.join(mpTestDirPath, dirname)
+        return packageNameList.map((pkgname) => {
+          return exec(`pnpm link --global ${pkgname}`, {
+            ...execEnvOptions,
+            cwd: mpTestProjectPath,
+          })
+        })
+      })
+      .flat()
+  )
+  log(linkPkgResults)
+
+  done()
 }
